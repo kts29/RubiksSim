b90fa8611d67f4334626077b7d02ea37d6cdbb81
kts29 <146642585+kts29@users.noreply.github.com>
2025-06-29 11:57:03 +0530

    Added all fundamental functions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RubiksSim
 Rubiks cube simulation with logic in cpp and visualization in pythonOpenGl. Not completed Yet
+
+
+<!-- 
+git commands
+cd path/to/your/folder
+git init
+git add .
+git commit -m "Initial commit of C++ Rubik's Cube simulator"
+git remote add origin https://github.com/yourusername/your-repo-name.git
+git push -u origin master 
+-->
--- rubiksSim.cpp
+++ rubiksSim.cpp
@@ -16,6 +16,7 @@ int getOppositeFace(int face) {
         default: return -1; // invalid
     }
 }
+// Basic Rotations
 void rotateCW(int sideNo){
     //first corners
     string temp = cube[sideNo][0][0];
@@ -37,6 +38,7 @@ void rotateCCW(int sideNo){
         rotateCW(sideNo);
     }
 }
+// Existing Functions
 void M() {
     cout<<"Executing M()..."<<endl;
     vector<string> tempUp = {cube[0][0][1], cube[0][1][1], cube[0][2][1]}; // middle column of Up face
@@ -55,6 +57,9 @@ void M() {
     cube[1][0][1] = tempUp[0];
     cube[1][1][1] = tempUp[1];
     cube[1][2][1] = tempUp[2];
+}
+void Mprime(){
+    M();M();M();
 }
 void R(){
     cout<<"Executing R()..."<<endl;
@@ -77,6 +82,9 @@ void R(){
     rotateCW(4);
 
 }
+void Rprime(){
+    R();R();R();
+}
 void L(){
     cout<<"Executing L()..."<<endl;
     vector<string> tempUp = {cube[5][0][0], cube[5][1][0], cube[5][2][0]}; // left column of up face
@@ -97,6 +105,9 @@ void L(){
     cube[1][1][2] = tempUp[1];
     cube[1][2][2] = tempUp[2];
     rotateCW(2);
+}
+void Lprime(){
+    L();L();L();
 }
 void U(){
     cout<<"Executing U()..."<<endl;
@@ -119,6 +130,9 @@ void U(){
     cube[1][0][2] = temp[2];
     rotateCCW(5);
 }
+void Uprime(){
+    U();U();U();
+}
 void showCube(){
     cout<<"Showing Cube..."<<endl;
     for(int i = 0;i<6;i++){
@@ -133,6 +147,173 @@ void showCube(){
     cout<<"Cube Shown"<<endl;
     cout<<"-- -- --"<<endl;
 }
+// Front Face Moves
+void F() {
+    cout << "Executing F()..." << endl;
+    vector<string> tempUp = {cube[0][2][0], cube[0][2][1], cube[0][2][2]}; // bottom row of Up face
+    // Move Left face (2) to Up face
+    cube[0][2][0] = cube[2][2][2];
+    cube[0][2][1] = cube[2][1][2];
+    cube[0][2][2] = cube[2][0][2];
+    // Move Down face (5) to Left face
+    cube[2][0][2] = cube[5][0][0];
+    cube[2][1][2] = cube[5][0][1];
+    cube[2][2][2] = cube[5][0][2];
+    // Move Right face (4) to Down face
+    cube[5][0][0] = cube[4][2][0];
+    cube[5][0][1] = cube[4][1][0];
+    cube[5][0][2] = cube[4][0][0];
+    // Move saved temp (Up face row) to Right face
+    cube[4][0][0] = tempUp[0];
+    cube[4][1][0] = tempUp[1];
+    cube[4][2][0] = tempUp[2];
+    rotateCW(1);
+}
+void Fprime() {
+    F(); F(); F();
+}
+
+// Back Face Moves
+void B() {
+    cout << "Executing B()..." << endl;
+    vector<string> tempUp = {cube[0][0][2], cube[0][0][1], cube[0][0][0]}; // top row of Up face (reversed)
+    // Move Right face to Up face
+    cube[0][0][2] = cube[4][2][2];
+    cube[0][0][1] = cube[4][1][2];
+    cube[0][0][0] = cube[4][0][2];
+    // Move Down face to Right face
+    cube[4][0][2] = cube[5][2][2];
+    cube[4][1][2] = cube[5][2][1];
+    cube[4][2][2] = cube[5][2][0];
+    // Move Left face to Down face
+    cube[5][2][2] = cube[2][2][0];
+    cube[5][2][1] = cube[2][1][0];
+    cube[5][2][0] = cube[2][0][0];
+    // Move saved temp to Left face
+    cube[2][0][0] = tempUp[2];
+    cube[2][1][0] = tempUp[1];
+    cube[2][2][0] = tempUp[0];
+    rotateCW(3);
+}
+void Bprime() {
+    B(); B(); B();
+}
+
+// Down Face Moves 
+void D() {
+    cout << "Executing D()..." << endl;
+    vector<string> tempFront = {cube[1][2][0], cube[1][2][1], cube[1][2][2]}; // bottom row of Front face
+    // Move Left face to Front face
+    cube[1][2][0] = cube[2][2][0];
+    cube[1][2][1] = cube[2][2][1];
+    cube[1][2][2] = cube[2][2][2];
+    // Move Back face to Left face
+    cube[2][2][0] = cube[3][2][0];
+    cube[2][2][1] = cube[3][2][1];
+    cube[2][2][2] = cube[3][2][2];
+    // Move Right face to Back face
+    cube[3][2][0] = cube[4][2][0];
+    cube[3][2][1] = cube[4][2][1];
+    cube[3][2][2] = cube[4][2][2];
+    // Move saved temp to Right face
+    cube[4][2][0] = tempFront[0];
+    cube[4][2][1] = tempFront[1];
+    cube[4][2][2] = tempFront[2];
+    rotateCW(5);
+}
+void Dprime() {
+    D(); D(); D();
+}
+
+// Middle Slice Moves (second layer)
+void E() {
+    cout << "Executing E()..." << endl;
+    vector<string> tempFront = {cube[1][1][0], cube[1][1][1], cube[1][1][2]}; // middle row of Front face
+    // Move Left face to Front face
+    cube[1][1][0] = cube[2][1][0];
+    cube[1][1][1] = cube[2][1][1];
+    cube[1][1][2] = cube[2][1][2];
+    // Move Back face to Left face
+    cube[2][1][0] = cube[3][1][0];
+    cube[2][1][1] = cube[3][1][1];
+    cube[2][1][2] = cube[3][1][2];
+    // Move Right face to Back face
+    cube[3][1][0] = cube[4][1][0];
+    cube[3][1][1] = cube[4][1][1];
+    cube[3][1][2] = cube[4][1][2];
+    // Move saved temp to Right face
+    cube[4][1][0] = tempFront[0];
+    cube[4][1][1] = tempFront[1];
+    cube[4][1][2] = tempFront[2];
+}
+
+void Eprime() {
+    E(); E(); E();
+}
+
+// S
+void S() {
+    cout << "Executing S()..." << endl;
+    vector<string> tempUp = {cube[0][1][0], cube[0][1][1], cube[0][1][2]}; // middle row of Up face
+    // Move Left face to Up face
+    cube[0][1][0] = cube[2][2][1];
+    cube[0][1][1] = cube[2][1][1];
+    cube[0][1][2] = cube[2][0][1];
+    // Move Down face to Left face
+    cube[2][0][1] = cube[5][1][0];
+    cube[2][1][1] = cube[5][1][1];
+    cube[2][2][1] = cube[5][1][2];
+    // Move Right face to Down face
+    cube[5][1][0] = cube[4][2][1];
+    cube[5][1][1] = cube[4][1][1];
+    cube[5][1][2] = cube[4][0][1];
+    // Move saved temp to Right face
+    cube[4][0][1] = tempUp[2];
+    cube[4][1][1] = tempUp[1];
+    cube[4][2][1] = tempUp[0];
+}
+
+void Sprime() {
+    S(); S(); S();
+}
+
+// xyz
+void x() {
+    R();
+    Lprime();
+    Mprime();
+}
+
+void xprime() {
+    Rprime();
+    L();
+    M();
+}
+
+void y() {
+    U();
+    Dprime();
+    Eprime();
+}
+
+void yprime() {
+    Uprime();
+    D();
+    E();
+}
+
+void z() {
+    F();
+    Bprime();
+    S();
+}
+
+void zprime() {
+    Fprime();
+    B();
+    Sprime();
+}
+//Cube functions
 void resetCube(){
     cout<<"Resetting Cube..."<<endl;
     for(int i = 0;i<6;i++){
@@ -161,9 +342,27 @@ void resetCubeCheckered() {
 int main(){
     resetCubeCheckered();
     showCube();
-    M();
+    
+    cout << "\nTesting fundamental cube moves:\n";
+    
+    F();
+    showCube();
+    B();
+    showCube();
+    D();
     showCube();
     R();
     showCube();
+    L();
+    showCube();
+    U();
+    showCube();
+    M();
+    showCube();
+    E();
+    showCube();
+    S();
+    showCube();
+    
     return 0;
 }
